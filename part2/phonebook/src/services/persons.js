a20d4812624d2e1450a0129840cce06a9ab3cea9
jcasben <jcasben02@gmail.com>
2025-04-16 20:15:15 +0200

    [NEW]: created service for managing persons in backend

---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const BASE_URL = 'http://localhost:3001/persons';
+
+const getAllPersons = () => {
+    const request = axios.get(BASE_URL);
+    return request.then(response => response.data);
+}
+
+const createPerson = newPerson => {
+    const request = axios.post(BASE_URL, newPerson);
+    return request.then(response => response.data);
+}
+
+const deletePerson = id => {
+    const request = axios.delete(`${BASE_URL}/${id}`);
+    return request.then(response => response.data);
+}
+
+const updatePerson = (id, newPerson) => {
+    const request = axios.put(`${BASE_URL}/${id}`, newPerson);
+    return request.then(response => response.data);
+}
+
+export default {getAllPersons, createPerson, updatePerson, deletePerson}
